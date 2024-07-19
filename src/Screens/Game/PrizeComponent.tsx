02a8dfaa9414579e7d8fcb0846f726467bdeaa04
nandobfer <nandobfer@yahoo.com.br>
2024-07-18 21:44:14 -0300

    update: customer corrections

--- src/Screens/Game/PrizeComponent.tsx
+++ src/Screens/Game/PrizeComponent.tsx
@@ -25,16 +25,12 @@ export const PrizeComponent: React.FC<PrizeComponentProps> = ({ prize_number, se
                         borderRadius: 10,
                         borderBottomWidth: 5,
                         borderColor: colors.primary,
-                        position: "relative",
                     },
                     ORIENTATION == "desktop" && { padding: 30, borderBottomWidth: 10 },
-                    selected && { borderBottomColor: colors.success },
+                    selected && { backgroundColor: colors.success, borderBottomColor: colors.success },
                 ]}
             >
                 <Text style={[{ color: colors.background, fontSize: 30 }, ORIENTATION == "desktop" && { fontSize: 40 }]}>{prize_number}</Text>
-                <View style={[{ position: "absolute", right: 0, top: 0 }]}>
-                    <RadioButton.Android value="" status={selected ? "checked" : "unchecked"} color={colors.success} />
-                </View>
             </Surface>
         </TouchableRipple>
     )
